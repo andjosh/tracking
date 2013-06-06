0668bf117cca702f90b86a8e83f583741f4a63b6
j-beckman <beckman.joshua@gmail.com>
2013-06-05 21:07:56 -0500

    Ready for heroku and year counter added.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,23 +1,13 @@
 window.onload = function() {
 
-  var timesTemps = [];
   var socket = io.connect(window.location.hostname);
-  var infoBox = document.getElementById("infoBox");
+  var x = document.getElementById("age");
 
-  socket.on('timeTemp', function (data) {
-      if(data.message) {
-          timesTemps.push(data);
-          var html = '';
-          for(var i=0; i<timesTemps.length; i++) {
-              html += timesTemps[i].message + '<br />';
-          }
-          infoBox.innerHTML = html;
+  socket.on('newAge', function (data) {
+      if(data.years) {
+          var html = data.years;
+          x.innerHTML = html;
       }
-      else if(data.tm) {
-          var html = '';
-          html += data.tp + ' at ' + data.tm + '<br />';
-          infoBox.innerHTML = html;
-      } 
       else {
           console.log("There is a problem with the infoBox:", data);
       }
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,20 @@
 
 exports.index = function(io) {
 	return function(req,res) {
+		console.log('User is ' + req.user)
+		if (req.user)  {
+			var userAge = new Date(req.user.birthdate);
+			function newAge() {
+				var now = new Date();
+		    var aged = Math.round((now - userAge)/(1000*60*60*24*7*52)*10000000)/10000000;
+			  io.sockets.emit('newAge', { years: aged });
+			  ageTimer();
+			};
+			function ageTimer() {
+				setTimeout(newAge, 1000);
+			};
+			ageTimer();
+		}
 		res.render('index', { title: 'Track Anything', user: req.user });
   };
 };
